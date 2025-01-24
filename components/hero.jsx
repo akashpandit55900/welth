7cb976db22849b8e799b8cc70c37c433c9109fe5
akashpandit55900 <akashpandit55900@gmail.com>
2025-01-24 11:01:08 +0530

    completed landing page

---
+++ components/hero.jsx
@@ -0,0 +1,77 @@
+"use client";
+
+import React, { useEffect, useRef } from "react";
+import Image from "next/image";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+
+const HeroSection = () => {
+  const imageRef = useRef();
+
+  useEffect(() => {
+    const imageElement = imageRef.current;
+
+    const handleScroll = () => {
+      const scrollPosition = window.scrollY;
+      const scrollThreshold = 100;
+
+      if (scrollPosition > scrollThreshold) {
+        imageElement.classList.add("scrolled");
+      } else {
+        imageElement.classList.remove("scrolled");
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
+  return (
+    <div className="pb-20 px-4">
+      <div className="container mx-auto text-center">
+        {/* Heading */}
+        <h1 className="text-5xl md:text-8xl lg:text-[105px] pb-6 gradient-title">
+          Manage Your Finances <br /> with Intelligence
+        </h1>
+
+        {/* Some Information */}
+        <p className="text-xl text-gray-600 mb-8 max-w-2xl mx-auto">
+          An AI-powered financial management platform that helps you track,
+          analyze, and optimize your spending with real-time insights.
+        </p>
+
+        {/* Action Buttons to start*/}
+        <div className="flex justify-center space-x-4">
+          <Link href="/dashboard">
+            <Button size="lg" className="px-8">
+              Get Started
+            </Button>
+          </Link>
+
+            {/* Link to edit latter on after pushing code to github */}
+          <Link href="https://www.youtube.com/roadsidecoder">
+            <Button size="lg" variant="outline" className="px-8">
+              Source Code
+            </Button>
+          </Link>
+        </div>
+
+        {/* Banner Image on Landing Page */}
+        <div className="hero-image-wrapper">
+          <div ref={imageRef} className="hero-image">
+            <Image
+              src="/banner.jpeg"
+              width={1280}
+              height={720}
+              alt="Dashboard Preview"
+              className="rounded-lg shadow-2xl border mx-auto"
+              priority
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default HeroSection;
