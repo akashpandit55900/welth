0aecc6a78b149cb6a0d27ff7ebf0fdecd32d8fc2
akashpandit55900 <akashpandit55900@gmail.com>
2025-01-25 21:01:49 +0530

    Added Monthly Budget Feature

--- app/(main)/dashboard/page.jsx
+++ app/(main)/dashboard/page.jsx
@@ -4,14 +4,29 @@ import { Card, CardContent } from "@/components/ui/card";
 import { Plus } from "lucide-react";
 import React from "react";
 import AccountCard from "./_components/account-card";
+import { getCurrentBudget } from "@/actions/budget";
+import BudgetProgress from "./_components/budget-progress";
 
 async function DashboardPage() {
   const accounts = await getUserAccounts();
 
-  return (
-    <div className="px-5">
-      {/* Budget Progress */}
+  const defaultAccount = accounts?.find((account) => account.isDefault);
+  // Get budget for default account
+  let budgetData = null;
+  if (defaultAccount) {
+    budgetData = await getCurrentBudget(defaultAccount.id);
+  }
 
+  console.log(budgetData);
+  return (
+    <div className="space-y-8">
+      {/* Budget Progress */}
+      {defaultAccount && (
+        <BudgetProgress
+          initialBudget={budgetData?.budget}
+          currentExpenses={budgetData?.currentExpenses || 0}
+        />
+      )}
       {/* Overview */}
 
       {/* Accounts Grid */}
@@ -27,7 +42,7 @@ async function DashboardPage() {
 
         {accounts.length > 0 &&
           accounts?.map((account) => {
-            return <AccountCard key={account.id} account={account} />
+            return <AccountCard key={account.id} account={account} />;
           })}
       </div>
     </div>
